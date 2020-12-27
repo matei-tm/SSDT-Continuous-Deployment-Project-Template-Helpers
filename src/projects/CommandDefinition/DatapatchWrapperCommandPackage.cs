23cdc188b0c05b578768114c786af39c508b31ff
Mircea MATEI <matei_tm@yahoo.com>
2020-12-27 03:54:56 +0200

    11. Add new command for change promoter

--- src/projects/CommandDefinition/DatapatchWrapperCommandPackage.cs
+++ src/projects/CommandDefinition/DatapatchWrapperCommandPackage.cs
@@ -11,6 +11,7 @@ PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
+using System.Threading;
 using Microsoft.VisualStudio.Shell;
 
 namespace DatapatchWrapper
@@ -24,5 +25,22 @@ namespace DatapatchWrapper
     public sealed class DatapatchWrapperCommandPackage : AsyncPackage
     {
         public const string PackageGuidString = "bbdecb2e-eb7f-4144-80c2-2cb7ab70712a";
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async System.Threading.Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await base.InitializeAsync(cancellationToken, progress);
+
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await ChangePromoter.InitializeAsync(this);
+        }
     }
 }
